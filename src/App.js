06d7694829cd228d55472def02d931b93de9da20
Daniel <don@Don.Don>
2021-04-23 17:37:00 +0100

    solved problem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import all pages from fintech design
-import FintechDesign from "./Projects/index"
-import Subscriptions from "./Projects/subscriptions"
-import Notifications from "./Projects/notifications"
-import Cards from "./Projects/cards"
-import NewCard from "./Projects/newCard"
-import Profile from "./Projects/profile"
+import FintechDesign from "./Projects/fintech/index"
+import Subscriptions from "./Projects/fintech/subscriptions"
+import Notifications from "./Projects/fintech/notifications"
+import Cards from "./Projects/fintech/cards"
+import NewCard from "./Projects/fintech/newCard"
+import Profile from "./Projects/fintech/profile"
 
 
 import { Switch, Route } from "react-router-dom"
